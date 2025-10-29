8b66f7b5616dd8b80d47dd86398a7ed2691f3c92
LisAY22 <lisajxupyax@gmail.com>
2025-10-28 21:59:01 -0600

    feat: backend
    This commit contains the implementation of the API.

--- BACKEND/todo/views.py
+++ BACKEND/todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from todo.models import TodoItem
+from rest_framework import viewsets
+from todo.serializers import TodoItemSerializer
 
-# Create your views here.
+# ViewSets define the view behavior.
+# Shortcut from Django Rest Framework (DRF) that automatically builds all the API endpoints
+class TodoItemViewSet(viewsets.ModelViewSet):
+    # Tells the viewset what data to work with and how to serialize it
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
